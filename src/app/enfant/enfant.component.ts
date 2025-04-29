b76ef8991b9343f4c84dce3c04a7c7507232a6cd
Raidel-28 <llefebvre2838@outlook.fr>
2025-04-29 10:15:23 +0200

    first commit

---
+++ src/app/enfant/enfant.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enfant',
+  imports: [],
+  templateUrl: './enfant.component.html',
+  styleUrl: './enfant.component.css'
+})
+export class EnfantComponent {
+
+}
